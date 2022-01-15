50e2b00f0fdc9990bc5bc502ebdabfe77c8367fd
Mihai Berar <mihai.berar@accesa.eu>
2022-01-15 15:57:27 +0200

    Add media query

--- dentistiq/src/components/footer/Footer.js
+++ dentistiq/src/components/footer/Footer.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
+const program = [
+  'Luni, Miercuri și Joi: 15:00 - 21:00',
+  'Marți: 09:00 - 15:00',
+];
+
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <h1>Program:</h1>
+      <h1>
+        <u className={styles.programTitle}>Program:</u>
+      </h1>
       <div className={styles.program}>
-        <h2>Luni, Miercuri si Joi: 15:00 - 21:00</h2>
-        <h2>Marti: 09:00 - 15:00</h2>
+        {program.map((schedule, index) => {
+          return <h2 key={index}>{schedule}</h2>;
+        })}
       </div>
     </div>
   );
--- dentistiq/src/components/navBar/NavBar.js
+++ dentistiq/src/components/navBar/NavBar.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styles from './NavBar.module.css';
 import logo from '../../images/simple-logo.jpeg';
+import facebook from '../../images/Facebook.png';
+import Button from '../button/Button';
+
+const buttons = ['Acasă', 'Despre noi', 'Servicii', 'Galerie', 'Contact'];
 
 const NavBar = () => {
   return (
     <div className={styles.navBar}>
       <img className={styles.img} src={logo} alt='Logo' />
-      <button className={styles.button}>Acasa</button>
-      <button className={styles.button}>Despre noi</button>
-      <button className={styles.button}>Servicii</button>
-      <button className={styles.button}>Galerie</button>
-      <button className={styles.button}>Contact</button>
-      <button className={styles.button}>FB</button>
+      {buttons.map((button, index) => {
+        return <Button key={index} buttonText={button} />;
+      })}
+      <a
+        href='https://www.facebook.com/dentistiq.ro'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img className={styles.facebookIcon} src={facebook} alt='Facebook' />
+      </a>
     </div>
   );
 };
