621c661264ae086941864a6a0a7a0711ed0f7dc5
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2022-02-01 15:36:24 +0200

    Finished styling map in contacting page

--- dentistiq/src/components/contactPage/contactMap/contactMap.js
+++ dentistiq/src/components/contactPage/contactMap/contactMap.js
@@ -4,51 +4,22 @@ import styles from './contactMap.module.css'
 
 const Map = () =>{
     return(
-        <>
-        {/* <div class="mapouter">
+        // <div class="mapouter">
+            // <div class="gmap_canvas">
                 <iframe 
-                    // width="700" 
-                    width="100%"
-                    // height='80 vh'
-                    height="100%"
-                    // min-height="400"
+                    src="https://maps.google.com/maps?q=Baladei%201A%20Turda&t=k&z=17&ie=UTF8&iwloc=&output=embed" 
+                    className="iframe_class"
                     id="gmap_canvas" 
-                    src="https://maps.google.com/maps?q=Baladei%201A%20Turda&t=&z=13&ie=UTF8&iwloc=&output=embed" 
                     frameborder="0" 
                     scrolling="no" 
                     marginheight="0" 
                     marginwidth="0">
                 </iframe>
-        </div> */}
-        <>
-            <div class="mapouter">
-                <div class="gmap_canvas">
-                    <iframe 
-                        width="700" 
-                        height="500" 
-                        id="gmap_canvas" 
-                        src="https://maps.google.com/maps?q=Baladei%201A%20Turda&t=k&z=17&ie=UTF8&iwloc=&output=embed" 
-                        frameborder="0" 
-                        scrolling="no" 
-                        marginheight="0" 
-                        marginwidth="0"
-                    >
-                    </iframe>
-                    {/* <a href="https://www.whatismyip-address.com/divi-discount/">where are you??</a> */}
-                    <br/>
-                    {/* <style>
-                        .mapouter{position:relative;text-align:right;height:500px;width:700px;}
-                    </style> */}
-                    {/* <a href="https://www.embedgooglemap.net">google iframe map</a> */}
-                    {/* <style>
-                        {.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:700px;}}
-                    </style> */}
-                </div>
-            </div>
-        </>
-
-        </>
+            // </div>
+        // </div>
     )
 }
 
 export default Map;
+
+
