5316aff6c0a26ca55343c4b532e7a83e616df2f6
alexcorsau <77393961+alexcorsau@users.noreply.github.com>
2023-01-04 10:14:21 +0200

    corrected name

--- dentistiq/src/components/despreNoi/DespreNoi.js
+++ dentistiq/src/components/despreNoi/DespreNoi.js
@@ -6,7 +6,7 @@ const DespreNoi = () => {
     <div className={styles.despreNoi}>
       <h3 className={styles.title}>Despre noi</h3>
       <h4 className={styles.text}>
-        La Dentistiq vrem să demonstrăm că vizita la stomatolog poate fi o
+        La Dentistiq Implant vrem să demonstrăm că vizita la stomatolog poate fi o
         experiență plăcută, care nu va mai fi privită cu groază sau emoție. În
         fiecare locație Dentistiq vei găsi aceleași standarde de calitate a
         actului medical și a materialelor utilizate, accentul pus pe igienă și
